3eb0098ee3c06bad8822b5346adb80630b324561
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-09-26 11:32:24 +0530

    Adding the login UI

--- src/components/UI/SubmitBtn.jsx
+++ src/components/UI/SubmitBtn.jsx
@@ -1,8 +1,17 @@
+import { useNavigation } from "react-router-dom"
+
 /* eslint-disable react/prop-types */
 const SubmitBtn = ({text}) => {
+  const navigation=useNavigation();
+  const isSubmitting=navigation.state==='submitting'
+
   return (
-    <button type="submit" className="btn btn-primary">
-        {text}
+    <button type="submit" className="btn btn-primary btn-block" disabled={isSubmitting}>
+        {
+            isSubmitting ? <>
+                <span className="loading loading-spinner"></span>
+            </> : text || 'Submit'
+        }
     </button>
   )
 }
